10d5ef5c965e23f9d9cb27fd55a8103c8617ad9b
Luiz Guilherme <guilhermecostakl@gmail.com>
2023-04-25 17:04:00 -0300

    Olá, Mundo!

---
+++ primeiroprograma.c
@@ -0,0 +1,9 @@
+# include <stdio.h>
+int main() {
+    printf("Ola mundo!");
+    return 0;
+}
+/*
+void - usada quando a função não retorna nenhum valor
+tipo nome(){} - usado para retornar valores
+*/
